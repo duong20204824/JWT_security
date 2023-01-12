94edec1d4f132ba2d3ea32ce04fb88f65fd3a357
duong <duong.tn204824@sis.hust.edu.vn>
2023-01-12 15:06:22 +0700

    project jwt

---
+++ src/main/java/duong/JWT_sercurity/JwtSercurityApplication.java
@@ -0,0 +1,13 @@
+package duong.JWT_sercurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSercurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSercurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/duong/JWT_sercurity/JwtSercurityApplicationTests.java
@@ -0,0 +1,13 @@
+package duong.JWT_sercurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSercurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
